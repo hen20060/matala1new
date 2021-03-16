e7b4a7e0d1d9f6295d897eca8c6d4837e53ae79b
Hen20060 <chensimon20060@gmail.com>
2021-03-16 17:54:15 +0200

    matala1

---
+++ equations.py
@@ -0,0 +1,57 @@
+def exponent(x):
+    result=1
+    for n in range(1,160):
+        result = result + helper2(x, n)/helper(n)
+    return result
+def helper(n):
+    x=1
+    for i in range(int(n)):
+        x=x*(i+1)
+    return x
+
+def abs1(x):
+    if x<0:
+        x=x*(-1)
+    return x
+
+def helper2(x, n):
+    temp =1
+    if int(n) == 0:
+        return 1
+    if int(n) == 1:
+        return x
+    for i in range(n):
+        temp = temp * x
+    return temp
+
+def ln(x):
+    epsilon = 0.001
+    yn=x-1.0
+    yn1=yn + 2*(x-exponent(yn))/(x+exponent(yn))
+    while abs1(yn-yn1)>epsilon:
+        yn=yn1
+        yn1=yn + 2*(x-exponent(yn))/(x+exponent(yn))
+    return yn1    
+
+def XtimesY( x , y ):    
+    if x>0:      
+        lnx = ln(x)
+        power=lnx*y
+        result= exponent(power)
+    else:
+        result=0.0
+    return result
+
+def sqrt(x,y):
+    return (XtimesY(y,1/x))
+
+def colculate(x):
+    if(x<=0):
+        result=0
+    else:
+        e=2.718281828459045
+        result= XtimesY(e, x)*XtimesY(7, x)*XtimesY(x, -1)*sqrt(x, x)
+        result = float('%0.6f' % result)
+    return result
+x = float(input("Enter number: "))
+print(colculate(x))
